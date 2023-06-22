83ea0089c585172a52fe2ac4ff961fa8e9e269e7
G-gold <111223856+G-gold@users.noreply.github.com>
2023-06-22 11:44:15 +0100

    Image and other changes made

---
+++ script.js
@@ -0,0 +1,21 @@
+// let numberOfBottles= 99
+// while (numberOfBottles >=0){
+//     let bottleWord = "bottle"
+//     if(numberOfBottles === 1){
+//         bottleWord ="bottles"
+//     }
+//     let cot = numberOfBottles--;
+//     console.log(`${numberOfBottles} ${bottleWord} of beer on the wall, ${numberOfBottles} ${bottleWord} of beer.
+// Take one down and pass it around,  ${cot} ${bottleWord} of beer on the wall`);
+//     cot
+// }
+
+// for(i=2;i<=5;i++){
+//     console.log(i);
+// }
+
+let a = 1
+while(a<=5){
+    console.log(a);
+    a++;
+}
